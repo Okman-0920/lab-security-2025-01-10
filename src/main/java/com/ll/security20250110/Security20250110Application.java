bd2e7e59dd0144c4e76011ba643a162f20f27f42
Okman-0920 <nohoksun94@gmail.com>
2025-01-10 15:41:23 +0900

    setting 1

---
+++ src/main/java/com/ll/security20250110/Security20250110Application.java
@@ -0,0 +1,13 @@
+package com.ll.security20250110;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security20250110Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security20250110Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/security20250110/Security20250110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.security20250110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security20250110ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
