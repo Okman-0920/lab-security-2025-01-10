5286dd110696122e0181a89f6173f18357e2dd06
Okman-0920 <nohoksun94@gmail.com>
2025-01-27 12:30:03 +0900

    로그인 시큐리티 설정 완료

--- src/main/java/com/ll/security_2025_01_10/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/ll/security_2025_01_10/global/security/CustomAuthenticationFilter.java
@@ -1,10 +1,13 @@
 package com.ll.security_2025_01_10.global.security;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.ll.security_2025_01_10.domain.member.member.entity.Member;
+import com.ll.security_2025_01_10.domain.member.member.service.MemberService;
 import com.ll.security_2025_01_10.global.rq.Rq;
 
 import jakarta.servlet.FilterChain;
@@ -16,11 +19,37 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+	private final MemberService memberService;
 	private final Rq rq;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		rq.setLogin("user1");
+		// /api/로 시작하는 요청이 아니라면 패스
+		if (!request.getRequestURI().contains("/api/")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String authorization = request.getHeader("Authorization");
+
+		// Authorization 헤더가 없거나 Bearer 로 시작하지 않는다면 패스
+		if (authorization == null || !authorization.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String apiKey = authorization.substring("Bearer ".length());
+
+		Optional<Member> opMember = memberService.findByApiKey(apiKey);
+
+		if (opMember.isEmpty()) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		Member member = opMember.get();
+
+		rq.setLogin(member.getUsername());
 
 		filterChain.doFilter(request, response);
 	}
